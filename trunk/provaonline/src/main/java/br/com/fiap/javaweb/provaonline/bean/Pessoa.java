1eb606511b8dbbc99abbf04db53bc8e9e9dcf955
carlosrgomes@gmail.com <carlosrgomes@gmail.com@f205c619-8e5a-239a-e63e-80266c729232>
2013-05-12 18:56:55 +0000

    carga inicial

---
+++ trunk/provaonline/src/main/java/br/com/fiap/javaweb/provaonline/bean/Pessoa.java
@@ -0,0 +1,50 @@
+package br.com.fiap.javaweb.provaonline.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pessoa")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	@Column
+	private String nome;
+	@Column
+	private String sobrenome;
+	
+	@OneToOne
+	private Ususario usuario;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public Ususario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Ususario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+	
+	
+}
---
+++ trunk/provaonline/src/main/java/br/com/fiap/javaweb/provaonline/servlet/CadastrarUsuarioServlet.java
@@ -0,0 +1,42 @@
+package br.com.fiap.javaweb.provaonline.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Servlet implementation class CadastrarUsuarioServlet
+ */
+@Component
+@WebServlet("/CadastrarUsuario")
+public class CadastrarUsuarioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CadastrarUsuarioServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getOutputStream().print("teste");
+	}
+
+}
