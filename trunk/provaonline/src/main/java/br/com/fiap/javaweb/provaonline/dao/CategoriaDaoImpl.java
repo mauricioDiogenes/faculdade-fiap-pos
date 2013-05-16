0f416360f3f446bad4821c5bb7e38c58c3206a84
xande.genka@gmail.com <xande.genka@gmail.com@f205c619-8e5a-239a-e63e-80266c729232>
2013-05-16 04:30:11 +0000

    Cadastro Categorias      -cadastro       -visualização de todos

---
+++ trunk/provaonline/src/main/java/br/com/fiap/javaweb/provaonline/dao/CategoriaDaoImpl.java
@@ -0,0 +1,46 @@
+package br.com.fiap.javaweb.provaonline.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import br.com.fiap.javaweb.provaonline.bean.Categoria;
+
+public class CategoriaDaoImpl implements GenericDAO<Categoria> {
+	private Session session;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.session = sessionFactory.openSession();
+	}
+
+	@Override
+	public List<Categoria> listAll() {
+		@SuppressWarnings("unchecked")
+		List<Categoria> list = session.createQuery("from Categoria").list();
+		return list;
+	}
+
+	@Override
+	public Categoria find(Long id) {
+		return (Categoria) session.get(Categoria.class, id);
+	}
+
+	@Override
+	public Categoria update(Categoria t) {
+		session.saveOrUpdate(t);
+		return t;
+	}
+
+	@Override
+	public Categoria save(Categoria t) {
+		session.saveOrUpdate(t);
+		return t;
+	}
+
+	@Override
+	public void delete(Categoria t) {
+		session.delete(t);
+	}
+
+}
