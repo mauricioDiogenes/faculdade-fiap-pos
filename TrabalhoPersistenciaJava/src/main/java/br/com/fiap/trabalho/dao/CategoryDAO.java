83aebfb1cc1ed5fc41d1e52a0d8d854c01cfc96a
xande.genka@gmail.com <xande.genka@gmail.com@f205c619-8e5a-239a-e63e-80266c729232>
2013-09-09 20:47:29 +0000

    Actor e Movie funcionando sem relacionamentos

--- TrabalhoPersistenciaJava/src/main/java/br/com/fiap/trabalho/dao/CategoryDAO.java
+++ TrabalhoPersistenciaJava/src/main/java/br/com/fiap/trabalho/dao/CategoryDAO.java
@@ -1,16 +1,10 @@
 package br.com.fiap.trabalho.dao;
 
-import br.com.fiap.trabalho.entity.Category;
-
 /***
  * Interface responsal por definir os metodos do {@link CategoryDAO}
  * @author carlosrgomes@gmail.com
  *
  */
 public interface CategoryDAO {
-	public Category createCategory(Category category);
-	
-	public void deleteCategory(Category category);
-	
-	public Category selectCategoryByName(String categoryName);
+
 }
