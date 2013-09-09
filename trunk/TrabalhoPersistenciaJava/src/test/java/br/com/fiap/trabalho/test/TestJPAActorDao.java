8f0f792ca80a8b12ff45442fd5550a64adf4dc29
xande.genka@gmail.com <xande.genka@gmail.com@f205c619-8e5a-239a-e63e-80266c729232>
2013-09-09 20:47:29 +0000

    Actor e Movie funcionando sem relacionamentos

--- trunk/TrabalhoPersistenciaJava/src/test/java/br/com/fiap/trabalho/test/TestJPAActorDao.java
+++ trunk/TrabalhoPersistenciaJava/src/test/java/br/com/fiap/trabalho/test/TestJPAActorDao.java
@@ -1,49 +1,75 @@
 package br.com.fiap.trabalho.test;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Date;
+import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.com.fiap.trabalho.dao.AbstractDAOFactory;
+import br.com.fiap.trabalho.dao.ActorDAO;
+import br.com.fiap.trabalho.dao.jpa.JPADAOFactory;
 import br.com.fiap.trabalho.entity.Actor;
 
 
 
 public class TestJPAActorDao {
-	
-	private EntityManagerFactory factory;
-	private EntityManager manager;
-	
+	private static ActorDAO actorDAO;
 	@Before
 	public void init(){
-		factory = Persistence .createEntityManagerFactory ("persistenceUnit");
-		manager = factory.createEntityManager ();
-		
-	}
-	
-	@After
-	public void close(){
-		factory.close ();
-	}
-	
-	@Test
-	public void validarcreateEntityManagerFactory(){
-		Assert.assertNotNull(this.factory);
-		Assert.assertNotNull(this.manager);
+		AbstractDAOFactory abstractDAOFactory = new JPADAOFactory();
+		actorDAO = abstractDAOFactory.createActorDAO();
+		Actor actor1 = new Actor();
+		actor1.setBirthDate(new Date("10/10/2010"));
+		actor1.setFullName("actor1");
+		actorDAO.createActor(actor1);
+
+		Actor actor2 = new Actor();
+		actor2.setBirthDate(new Date("10/10/2011"));
+		actor2.setFullName("actor2");
+		actorDAO.createActor(actor2);
 	}
 	
 	@Test
-	public void insert(){
-		Actor actor = new Actor();
-		actor.setBirthDate(new Date());
-		actor.setFullName("Teste");
-		manager.persist(actor);
+	public void validarcreateDao(){
+		Assert.assertNotNull(this.actorDAO);
 	}
 
+	
+	@Test
+	public void selectActorByName() {
+		List<Actor> actorList = actorDAO.selectActorByName("actor2");
+		assertNotNull(actorList);
+	}
+
+	@Test
+	public void insertActor() {
+		Actor actor = new Actor();
+		actor.setBirthDate(new Date("10/10/2000"));
+		actor.setFullName("actor insert");
+		actorDAO.createActor(actor);
+	}
+
+	@Test
+	public void selectActorByAge() {
+		List<Actor> actorList = actorDAO.selectActorByAge(2);
+		assertNotNull(actorList);
+	}
+
+	@Test
+	public void deleteActor() {
+		List<Actor> actorList = actorDAO.selectActorByName("actor1");
+		int idActor = actorList.get(0).getId();
+		actorDAO.deleteActor(actorList.get(0));
+		List<Actor> actorList2 = actorDAO.selectActorByName("actor1");
+		if(!actorList2.isEmpty()){
+			assertTrue(idActor != actorList2.get(0).getId());
+		}else{
+			assertTrue(true);
+		}
+	}
 }
--- trunk/TrabalhoPersistenciaJava/src/test/java/br/com/fiap/trabalho/test/TestJPAMovieDao.java
+++ trunk/TrabalhoPersistenciaJava/src/test/java/br/com/fiap/trabalho/test/TestJPAMovieDao.java
@@ -3,56 +3,38 @@ package br.com.fiap.trabalho.test;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import org.junit.BeforeClass;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.fiap.trabalho.dao.AbstractDAOFactory;
-import br.com.fiap.trabalho.dao.ActorDAO;
 import br.com.fiap.trabalho.dao.MovieDAO;
 import br.com.fiap.trabalho.dao.jpa.JPADAOFactory;
-import br.com.fiap.trabalho.entity.Actor;
 import br.com.fiap.trabalho.entity.Movie;
 
-public class MovieTest {
+public class TestJPAMovieDao {
+	private MovieDAO movieDAO;
 
-	private static MovieDAO movieDAO;
-	private static ActorDAO actorDAO;
-
-	@BeforeClass
-	public static void init() {
+	@Before
+	public void init() {
 		AbstractDAOFactory abstractDAOFactory = new JPADAOFactory();
 		movieDAO = abstractDAOFactory.createMovieDAO();
-		actorDAO = abstractDAOFactory.createActorDAO();
-
-		Actor actor1 = new Actor();
-		actor1.setBirthDate(new Date("10/10/2010"));
-		actor1.setFullName("actor1");
-		actorDAO.createActor(actor1);
-
-		Actor actor2 = new Actor();
-		actor2.setBirthDate(new Date("10/10/2011"));
-		actor2.setFullName("actor2");
-		actorDAO.createActor(actor2);
-		
-		List<Actor> actors = new ArrayList<Actor>();
-		actors.add(actor1);
-		actors.add(actor2);
-		
 		Movie movie = new Movie();
 		movie.setTitle("movie2");
 		movie.setYearr(2013);
-		movie.setActors(actors);
 		movieDAO.createMovie(movie);
 
 		Movie movie2 = new Movie();
 		movie2.setTitle("movieDel");
 		movie2.setYearr(2013);
-		movie.setActors(actors);
 		movieDAO.createMovie(movie2);
+	}
+
+	@Test
+	public void validarcreateDao() {
+		Assert.assertNotNull(this.movieDAO);
 	}
 
 	@Test
@@ -64,11 +46,6 @@ public class MovieTest {
 	@Test
 	public void selectMovieByYear() {
 		List<Movie> movieList = movieDAO.selectMoviesByYear(2013);
-		if(movieList != null){
-			Movie m = movieList.get(0);
-			List actors = m.getActors();
-			assertNotNull(actors);	
-		}
 		assertNotNull(movieList);
 	}
 
@@ -88,5 +65,4 @@ public class MovieTest {
 		List<Movie> movieList2 = movieDAO.selectMoviesByTitle("movieDel");
 		assertTrue(movieList2.isEmpty());
 	}
-
 }
